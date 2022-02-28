21e226558b95fd7277670eee9806edc7123a1364
Ahmed-Mahmoud-tech <ahmedmahmoudtech@gmail.com>
2022-02-28 18:35:39 +0200

    will contenu

---
+++ src/components/landing-components/LandingSecOne.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="sec-one">
+    <div class="sec-one-content container">
+      <h1>
+        اغتنم واحده من افضل عروض دكتور بى
+        <span>اليوم !</span>
+      </h1>
+      <p>
+        منتس حهتبس سحخيعخح خسيب خسحخي جخه خهسحخ خنكمكوش كنسى ةكمنبج كمنهطهيب كمي
+        ك0 بكمسيبس ةبس
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.sec-one {
+  direction: rtl;
+  background-image: url("../../assets/imgs/fgg-min.jpg");
+  height: 100vh;
+  background-size: cover;
+  //   color: #fff;
+  .sec-one-content {
+    text-align: center;
+    padding-top: 150px;
+    h1 {
+      margin-bottom: 30px;
+      @media (max-width: 992px) {
+        font-size: 52px;
+      }
+      @media (max-width: 576px) {
+        font-size: 36px;
+      }
+      span {
+        font-weight: 900;
+      }
+    }
+  }
+}
+</style>
---
+++ src/pages/landing-one.vue
@@ -0,0 +1,15 @@
+<template>
+  <landing-sec-one />
+  <content />
+</template>
+
+<script>
+import LandingSecOne from "src/components/landing-components/LandingSecOne.vue";
+import Content from "src/components/landing-components/Content.vue";
+export default {
+  components: { LandingSecOne, Content },
+  setup() {},
+};
+</script>
+
+<style></style>
